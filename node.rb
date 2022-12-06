d18b6fae595471162eb0d60e8ea1d1c821d7a5a9
Belbut <andreferreirabelbut@gmail.com>
2022-12-06 21:31:35 +0000

    Create Node class

---
+++ node.rb
@@ -0,0 +1,17 @@
+# Node class for Binary Search Trees
+# Includes Comparable Module, meaning we can directly compare between two Nodes
+class Node
+  include Comparable
+
+  attr_accessor :value, :child_left, :child_right
+
+  def initialize(value, child_left, child_right)
+    @value = value
+    @child_left = child_left
+    @child_right = child_right
+  end
+
+  def <=>(other)
+    @value <=> other.value
+  end
+end
